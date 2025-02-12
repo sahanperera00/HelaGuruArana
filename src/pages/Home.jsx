ff2b914eb34428d456102fbf06a57e16574c5443
Sahan Perera <sahanvimukthi00@gmail.com>
2025-02-12 19:35:05 +0530

    feat: added blog section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Button from "../components/Button";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function Home() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
     <div className="flex flex-col items-center">
       <div className="w-screen h-[80vh] flex justify-center py-8 bg-[url('/hero2.jpg')] bg-cover bg-center">
@@ -135,7 +157,106 @@ export default function Home() {
       </div>
 
       {/* Section 5 */}
-      <div className="w-full h-[800px]"></div>
+      <div className="w-screen flex justify-center py-20">
+        <div className="relative max-w-[80%] lg:max-w-[70%] w-full space-y-5">
+          <div className="flex flex-col lg:flex-row lg:justify-between space-y-10 lg:space-y-0">
+            <p className="text-4xl text-center lg:text-left lg:text-5xl font-semibold border-b-3 pb-3">
+              Blogs and Articles
+            </p>
+          </div>
+          <Carousel responsive={responsive} className="py-10" itemClass="px-5">
+            {/* Article 1 */}
+            <div className="flex flex-col space-y-3 bg-white rounded-3xl overflow-hidden shadow-lg">
+              <img
+                src="/blog1.jpg"
+                alt="Blog 1"
+                className="w-full h-[150px] object-cover"
+              />
+              <div className="space-y-3 bg-white px-6 pb-10">
+                <p className="w-fit border-b-3 pb-3 text-xl font-semibold">
+                  Blog Title #1
+                </p>
+                <p className="text-sm text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum tortor augue, vestibulum non felis sed.
+                </p>
+              </div>
+            </div>
+            {/* Article 2 */}
+
+            <div className="flex flex-col space-y-3 bg-white rounded-3xl overflow-hidden shadow-lg">
+              <img
+                src="/blog2.jpg"
+                alt="Blog 1"
+                className="w-full h-[150px] object-cover"
+              />
+              <div className="space-y-3 bg-white px-6 pb-10">
+                <p className="w-fit border-b-3 pb-3 text-xl font-semibold">
+                  Blog Title #2
+                </p>
+                <p className="text-sm text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum tortor augue, vestibulum non felis sed.
+                </p>
+              </div>
+            </div>
+            {/* Article 3 */}
+
+            <div className="flex flex-col space-y-3 bg-white rounded-3xl overflow-hidden shadow-lg">
+              <img
+                src="/blog3.jpg"
+                alt="Blog 1"
+                className="w-full h-[150px] object-cover"
+              />
+              <div className="space-y-3 bg-white px-6 pb-10">
+                <p className="w-fit border-b-3 pb-3 text-xl font-semibold">
+                  Blog Title #3
+                </p>
+                <p className="text-sm text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum tortor augue, vestibulum non felis sed.
+                </p>
+              </div>
+            </div>
+            {/* Article 4 */}
+
+            <div className="flex flex-col space-y-3 bg-white rounded-3xl overflow-hidden shadow-lg">
+              <img
+                src="/blog4.jpg"
+                alt="Blog 1"
+                className="w-full h-[150px] object-cover"
+              />
+              <div className="space-y-3 bg-white px-6 pb-10">
+                <p className="w-fit border-b-3 pb-3 text-xl font-semibold">
+                  Blog Title #4
+                </p>
+                <p className="text-sm text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum tortor augue, vestibulum non felis sed.
+                </p>
+              </div>
+            </div>
+            {/* Article 5 */}
+
+            <div className="flex flex-col space-y-3 bg-white rounded-3xl overflow-hidden shadow-lg">
+              <img
+                src="/blog5.jpg"
+                alt="Blog 1"
+                className="w-full h-[150px] object-cover"
+              />
+              <div className="space-y-3 bg-white px-6 pb-10">
+                <p className="w-fit border-b-3 pb-3 text-xl font-semibold">
+                  Blog Title #5
+                </p>
+                <p className="text-sm text-justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum tortor augue, vestibulum non felis sed.
+                </p>
+              </div>
+            </div>
+          </Carousel>
+        </div>
+      </div>
     </div>
   );
 }
